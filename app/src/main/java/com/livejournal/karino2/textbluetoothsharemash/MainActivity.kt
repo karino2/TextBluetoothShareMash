fb2dc120ee6c9b26864b4f55de0a21bb72ea742a
karino2 <hogeika2@gmailcom>
2018-02-20 22:27:58 +0900

    Add text send activity.

--- app/src/main/java/com/livejournal/karino2/textbluetoothsharemash/MainActivity.kt
+++ app/src/main/java/com/livejournal/karino2/textbluetoothsharemash/MainActivity.kt
@@ -6,6 +6,13 @@ import android.os.Environment
 import android.widget.Toast
 import java.io.File
 import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import android.support.v4.content.ContextCompat.startActivity
+import android.R.attr.exported
+import android.support.v4.content.FileProvider
+
 
 
 
@@ -23,16 +30,46 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
         setContentView(R.layout.activity_main)
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_share -> {
+                val documentPath = filesDir
+                val target = File(documentPath, FILE_NAME)
+                // if(target.exists()) { target.delete() }
+
+                val msg = (findViewById(R.id.editText) as EditText).text.toString()
+                target.writeText(msg)
+
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                val fileUri = FileProvider.getUriForFile(this,
+                        BuildConfig.APPLICATION_ID + ".provider",
+                        target)
+                intent.putExtra(Intent.EXTRA_STREAM, fileUri)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    val FILE_NAME = "text_bluetooth_share_mash.txt"
     val FILE_NAME_PAT = Regex("""text_bluetooth_share_mash(-[0-9]+)?\.txt""")
 
     fun showMessage(msg : String) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
+
 
     private fun insertLocalFileAndDelete() {
         val downloadPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
